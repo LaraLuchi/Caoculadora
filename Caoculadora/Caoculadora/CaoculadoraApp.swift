7407c31f7806bd59bb60f1cb4b4ccb81d5e34191
Lara Vieira Luchi <laraluchi@10-48-50-65.0200.ufpe.br.iosda.org>
2024-05-08 10:11:28 -0300

    Inicio caoculadora

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Lara Vieira Luchi on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
