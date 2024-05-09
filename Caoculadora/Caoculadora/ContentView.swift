20c2a826a8970fc0b08e31590b3e32770d991032
Lara Vieira Luchi <laraluchi@10-48-50-65.0200.ufpe.br.iosda.org>
2024-05-09 11:51:38 -0300

    Adicao de asset e botao

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
+    @State var result: Int?
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -18,7 +19,34 @@ struct ContentView: View {
             TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
             Text("Meses")
             TextField("E quantos meses além disso ele tem", value: $months, format: .number)
-              
+            Text("Porte")
+            //aqui vai o segmented control
+            
+            
+            
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            }
+            else {
+                Image(ImageResource.clarinha)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 150)
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+            
+            Button("Cãocular") {
+                print("cãocular")
+                result = 23
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .frame(height: 50)
+            .background(.indigo)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .bold()
+            
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
